5a4dc366b060780c6e93988756f0f31d065c042f
Nate Williams <nate@yogotech.com>
2022-03-13 22:11:32 -0600

    Hookup the DB and models so we can use them dynamically

--- src/main/kotlin/edu/carroll/cs495/Cs495Application.kt
+++ src/main/kotlin/edu/carroll/cs495/Cs495Application.kt
@@ -1,9 +1,15 @@
 package edu.carroll.cs495
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["edu.carroll.svc"])
+@EnableJpaRepositories(basePackages = ["edu.carroll.jpa.repo"])
+@EntityScan("edu.carroll.jpa")
 class Cs495Application
 
 fun main(args: Array<String>) {
