3c748c0df4d1f631f7ca0a3b6b0b93753de76bf7
Nate Williams <nate@yogotech.com>
2022-03-13 23:03:40 -0600

    Create a new page/template that fetches the list of users and displays the data

--- src/main/kotlin/edu/carroll/cs495/HtmlController.kt
+++ src/main/kotlin/edu/carroll/cs495/HtmlController.kt
@@ -1,16 +1,24 @@
 package edu.carroll.cs495
 
+import edu.carroll.svc.UserSvc
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class HtmlController {
+class HtmlController(val userSvc: UserSvc) {
 
     @GetMapping("/")
     fun index(model: Model): String {
         model["title"] = "CS495"
         return "index"
+    }
+
+    @GetMapping("/all")
+    fun getUsers(model: Model): String {
+        model["title"] = "All Users"
+        model["users"] = userSvc.fetchAllUsers()
+        return "users"
     }
 }
