e63bf5d32b88ec5b8f39baa55e3eab7e89b19249
Nate Williams <nate@yogotech.com>
2022-03-13 18:15:52 -0600

    Template from start.spring.io for a Kotlin/Gradle/Spring Boot app - See https://spring.io/guides/tutorials/spring-boot-kotlin/

---
+++ src/main/kotlin/edu/carroll/cs495/Cs495Application.kt
@@ -0,0 +1,11 @@
+package edu.carroll.cs495
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Cs495Application
+
+fun main(args: Array<String>) {
+	runApplication<Cs495Application>(*args)
+}
---
+++ src/test/kotlin/edu/carroll/cs495/Cs495ApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.carroll.cs495
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Cs495ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
