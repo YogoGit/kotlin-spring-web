3280813ff49f6c73f3a5c9221166ef3640642c1e
Nate Williams <nate@yogotech.com>
2022-03-13 22:58:49 -0600

    Simpler and more correct setup (scanning, etc..)

--- src/main/kotlin/edu/carroll/cs495/Cs495Application.kt
+++ src/main/kotlin/edu/carroll/cs495/Cs495Application.kt
@@ -1,13 +1,15 @@
 package edu.carroll.cs495
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootApplication
-@ComponentScan(basePackages = ["edu.carroll.svc"])
+@ComponentScan(basePackages = ["edu.carroll"])
+@Configuration
+@EnableAutoConfiguration
 @EnableJpaRepositories(basePackages = ["edu.carroll.jpa.repo"])
 @EntityScan("edu.carroll.jpa")
 class Cs495Application
